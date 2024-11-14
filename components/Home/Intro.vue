b80a2e68bc11a4343f81ce2da0bb2b6cd473f72b
Elzodxon Sharofaddinov <elzodxon@gmail.com>
2024-11-15 02:44:05 +0500

    feat: text

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -3,25 +3,24 @@
     <NuxtImg src="https://media.licdn.com/dms/image/v2/C4D03AQH0sUP-lP_k4Q/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1626026862909?e=1736985600&v=beta&t=tpBFf_7hetdtwyinKT-I4yXDu5ILlLDCP4AKFuFdA3E" alt="Elzodxon Sharofaddinov"
       class="ring-2 border ring-gray-200 border-gray-300 dark:ring-white/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-12 w-12 sm:h-16 sm:w-16"
       sizes="48px sm:64px" placeholder format="webp" />
-    <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100">
-      Hello!
-    </h1>
-    <p class="text-gray-900 dark:text-gray-400">
-      I'm Elzodxon, I am solving problems and building products in Tashkent, Uzbekistan. 
-    </p>
-    <p class="text-gray-900 dark:text-gray-400">
-      In this days, I'm building
-      <a href="https://aisha.group" target="_blank" class="underline">Aisha AI</a> and I am a freshman at <a href="https://aut-edu.uz" class="underline" target="_blank">
-        AUT(American University of Technology)
-      </a>. I love playing Chess, swimming and CS2.
-    </p>
+      <h1>About Me</h1>
+      <p class="text-gray-900 dark:text-gray-400">
+          I'm Elzodxon, and I solve problems and build products in Tashkent, Uzbekistan.
+      </p>
+      <p class="text-gray-900 dark:text-gray-400">
+          These days, I'm working with 
+          <a href="https://aisha.group" target="_blank" class="underline">Aisha AI</a> and am a freshman at 
+          <a href="https://aut-edu.uz" class="underline" target="_blank">AUT (American University of Technology)</a>. 
+          I love playing chess, swimming, and CS2.
+      </p>
+
   </div>
 </template>
 
 <script setup>
 useSeoMeta({
-  title: "Elzodxon Sharofaddinov",
-  description:
-    "I'm Elzodxon Sharofaddinov, in this days, I'm building AIsha AI and I am a student at AUT(American University of technology). I love playing chess, swimming and CS2.",
+    title: "Elzodxon Sharofaddinov",
+    description: "I'm Elzodxon Sharofaddinov. These days, I'm working with Aisha AI and am a student at AUT (American University of Technology). I love playing chess, swimming, and CS2.",
 });
 </script>
+
